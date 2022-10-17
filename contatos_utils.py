f98fe551dcb1a95b299442a5a892a16e02a3f1e9
Julio Facal <starlyn.dev@gmail.com>
2022-10-17 10:02:05 -0300

    trabalhando com arquivos pickle

--- contatos_utils.py
+++ contatos_utils.py
@@ -1,4 +1,4 @@
-import csv
+import csv, pickle
 from contato import Contato
 
 def csv_contatos(arquivo, encoding = 'latin_1'):
@@ -13,5 +13,15 @@ def csv_contatos(arquivo, encoding = 'latin_1'):
             
             contato = Contato(id, nome, email)
             contatos.append(contato)
+    
+    return contatos
+
+def pickle_contatos(contatos, arquivo):
+    with open(f'dados/{arquivo}.p', 'wb') as file:
+        pickle.dump(contatos, file)
+        
+def converter_pickle(arquivo):
+    with open(f'dados/{arquivo}.p', 'rb') as file:
+        contatos = pickle.load(file)
     
     return contatos
--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import contatos_utils
 
 try:
-    contatos = contatos_utils.csv_contatos('contatos')
+#    contatos = contatos_utils.csv_contatos('contatos')
+#    contatos_utils.pickle_contatos(contatos, 'contatos')
+
+    contatos = contatos_utils.converter_pickle('contatos')
     
     for contato in contatos:
         print(f'{contato.id} - {contato.nome} - {contato.email}')
