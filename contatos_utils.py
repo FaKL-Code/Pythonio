85c39a63ece2e63da89f8e033281eba237639707
Julio Facal <starlyn.dev@gmail.com>
2022-10-17 10:18:03 -0300

    trabalhando com arquivos json

--- contatos_utils.py
+++ contatos_utils.py
@@ -1,4 +1,4 @@
-import csv, pickle
+import csv, pickle, json
 from contato import Contato
 
 def csv_contatos(arquivo, encoding = 'latin_1'):
@@ -24,4 +24,23 @@ def converter_pickle(arquivo):
     with open(f'dados/{arquivo}.p', 'rb') as file:
         contatos = pickle.load(file)
     
+    return contatos
+
+def json_contatos(contatos, arquivo):
+    with open(f'dados/{arquivo}.json', 'w') as file:
+        json.dump(contatos, file, default = contato_json)
+        
+def contato_json(contato):
+    return contato.__dict__
+        
+def converter_json(arquivo):
+    contatos = []
+    
+    with open(f'dados/{arquivo}.json', 'r') as file:
+        contato_dicionario = json.load(file)
+        
+        for contato in contato_dicionario:
+            c = Contato(**contato)
+            contatos.append(c)
+  
     return contatos
--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import contatos_utils
 
 try:
-#    contatos = contatos_utils.csv_contatos('contatos')
+    contatos = contatos_utils.csv_contatos('contatos')
 #    contatos_utils.pickle_contatos(contatos, 'contatos')
 
-    contatos = contatos_utils.converter_pickle('contatos')
+#    contatos = contatos_utils.converter_pickle('contatos')
+#    contatos_utils.json_contatos(contatos, 'contatos')
+
+    contatos = contatos_utils.converter_json('contatos')
     
     for contato in contatos:
         print(f'{contato.id} - {contato.nome} - {contato.email}')
