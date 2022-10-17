b95a142f1ebc555fbea19fb10fa4f13b8562dd2e
Julio Facal <starlyn.dev@gmail.com>
2022-10-17 00:43:50 -0300

    Aprendendo tratamento de excecoes

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ try:
 
     for linha in file:
         print(linha)
-
+except FileNotFoundError:
+    print('arquivo nao encontrado')
+except PermissionError:
+    print('Sem permissao para escrever no diretorio')
 finally:
     file.close()
