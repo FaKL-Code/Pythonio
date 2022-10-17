11debcb8a30f58075138d118d97a9f536da632d1
Julio Facal <starlyn.dev@gmail.com>
2022-10-17 00:48:44 -0300

    aprendendo a usar o with para liberar recursos

--- concorrencia.py
+++ concorrencia.py
@@ -1,8 +1,10 @@
-file = open('dados/escrita.csv', 'w')
-arquivo = open('dados/escrita.csv', 'w')
-
 contato_a = '5, aaaaaaaaaaaaaaaaaaaaaaaaaaaa, aaaaaaaaaaaaaa@aaaaaaaaaaa.aaa.aa'
 contato_b = '6, bbbbbbbbbbbbbbbbbbbbbbbbbbbb, bbbbbbbbbbbbbb@bbbbbbbbbbb.bbb.bb'
+
+with open('dados/escrita.csv', 'w') as file:
+    file.write(contato_a)    
+with open('dados/escrita.csv', 'w') as arquivo:
+    arquivo.write(contato_b)
 
 file.write(contato_a)
 arquivo.write(contato_b)
