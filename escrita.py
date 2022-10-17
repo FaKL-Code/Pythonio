23db4c8d54d160b3720cf31afb5e4db97b54e0b0
Julio Facal <starlyn.dev@gmail.com>
2022-10-17 00:23:06 -0300

    aprendendo close() e flush()

--- escrita.py
+++ escrita.py
@@ -1,5 +1,12 @@
 file = open('dados/escrita.csv', 'a')
 
-contato = '4, dddddddddddddddddd, dddddddddddd@ddddddddd.ddd.dd\n'
+contato = ['4, dddddddddddddddddd, dddddddddddd@ddddddddd.ddd.dd\n']
+
+for pessoa in contato:
+    file.write(pessoa)
+
+file.flush()
 
 file.write(contato)
+
+file.close()
